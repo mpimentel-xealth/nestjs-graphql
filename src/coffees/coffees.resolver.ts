d3f8d20f1e3c1d41a00cf7c55459c9bfcc360414
Mason Pimentel <mason.pimentel@xealth.io>
2023-10-26 19:47:07 -0700

    Passing arg to resolver

--- src/coffees/coffees.resolver.ts
+++ src/coffees/coffees.resolver.ts
@@ -1,10 +1,16 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Coffee } from '../graphql';
+import { ParseIntPipe } from '@nestjs/common';
 
 @Resolver()
 export class CoffeesResolver {
   @Query('coffees')
   async findAll(): Promise<Coffee[]> {
     return [];
+  }
+
+  @Query('coffee')
+  async findOne(@Args('id', ParseIntPipe) id: number): Promise<Coffee> {
+    return null;
   }
 }
