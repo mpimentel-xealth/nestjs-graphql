045323573b0e0d0812fcbad1a05b7fa293c27860
Mason Pimentel <mason.pimentel@xealth.io>
2023-11-21 19:13:05 -0800

    Field resolvers

---
+++ src/coffees/coffee-flavors.resolver.ts
@@ -0,0 +1,23 @@
+import { Resolver, ResolveField, Parent } from '@nestjs/graphql';
+import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Resolver('Coffee')
+export class CoffeeFlavorsResolver {
+  constructor(
+    @InjectRepository(Flavor)
+    private readonly flavorsRepository: Repository<Flavor>,
+  ) {}
+
+  @ResolveField('flavors')
+  async getFlavorsOfCoffee(@Parent() coffee: Coffee) {
+    return this.flavorsRepository
+      .createQueryBuilder('flavor')
+      .innerJoin('flavor.coffees', 'coffees', 'coffees.id = :coffeeId', {
+        coffeeId: coffee.id,
+      })
+      .getMany();
+  }
+}
